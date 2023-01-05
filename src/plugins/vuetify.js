06d36f17824b1ab45b6b2b32559e70c2fb0a519d
Nicolas Pucci <nicolaspucci1989@gmail.com>
2023-01-05 18:43:38 -0300

    add brand colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,17 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#461e8c',
+                secondary: '#f6303e',
+                accent: colors.indigo.base, // #3F51B5
+            },
+        },
+    },
 });
