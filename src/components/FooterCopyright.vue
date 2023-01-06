41bcda054ab20d45c9d2aae8aa10e1239f2d800f
Nicolas Pucci <nicolaspucci1989@gmail.com>
2023-01-05 22:33:00 -0300

    minor changes

--- src/components/FooterCopyright.vue
+++ src/components/FooterCopyright.vue
@@ -1,7 +1,8 @@
 <template>
   <v-col class="text-center border-top pb-0">
     <v-col class="pb-0">Todos los derechos reservados © Basulado</v-col>
-    <v-col class="pb-0"> Desarrollado por <a class="text-decoration-underline" href="https://drubbit.com/"> Drubbit </a></v-col>
+    <v-col class="pb-0"> Desarrollado por <a class="text-decoration-underline" href="https://drubbit.com/"> Drubbit </a>
+    </v-col>
   </v-col>
 </template>
 
