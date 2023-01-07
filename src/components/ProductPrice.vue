55469c3f8fac5fd0609b918a2dee1b8ffe1c98c5
Nicolas Pucci <nicolaspucci1989@gmail.com>
2023-01-07 14:47:55 -0300

    add prev price

--- src/components/ProductPrice.vue
+++ src/components/ProductPrice.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="d-flex align-center">
+  <div>
+    <div class="grey--text text-decoration-line-through">
+      <span class="mr-1">$</span>150.772<sup>40</sup>
+    </div>
+    <div class="d-flex align-center">
       <span style="font-size: 1.9rem" class="font-weight-medium">
         <span class="mr-1">$</span>120.619<sup style="font-size: 60%">56</sup>
       </span>
-    <span class="red--text font-weight-bold text-subtitle-2 ml-2">
+      <span class="red--text font-weight-bold text-subtitle-2 ml-2">
           20% OFF
         </span>
+    </div>
   </div>
 </template>
 
