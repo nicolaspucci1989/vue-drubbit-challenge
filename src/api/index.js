3d42a73ae6fc9e0a53ce513efe40e315d2d225d4
Nicolas Pucci <nicolaspucci1989@gmail.com>
2023-01-06 23:02:40 -0300

    add api call

---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+// https://materialesbasualdo.com.ar/api/catalog/product/039033cd-f096-48ab-9950-0a33a1d2e441
+
+const BASE_URL = 'https://materialesbasualdo.com.ar/api'
+
+export async function getProduct(id) {
+  const response = await fetch(`${BASE_URL}/catalog/product/${id}`)
+  return response.json()
+}
