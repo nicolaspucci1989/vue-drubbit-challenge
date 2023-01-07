d28694f4448b1caeb3657aa750f28300f3272448
Nicolas Pucci <nicolaspucci1989@gmail.com>
2023-01-07 17:54:05 -0300

    product info

--- src/components/ProductInfo.vue
+++ src/components/ProductInfo.vue
@@ -2,8 +2,10 @@
   <v-row>
     <v-col cols="10">
       <h1>Inodoro Corto Ihcmjb Ferrum Marina</h1>
-      <div>SKU FER00048</div>
-      <div>FERRUM</div>
+      <v-chip color="grey lighten-4" text-color="grey darken-2" small class="font-weight-bold">SKU FER00048</v-chip>
+      <div class="text-uppercase mt-4 font-weight-bold" style="color: #461e8c">
+        Ferrum
+      </div>
     </v-col>
     <v-col cols="2" class="d-flex flex-column">
       <v-btn small depressed fab class="mb-2">
