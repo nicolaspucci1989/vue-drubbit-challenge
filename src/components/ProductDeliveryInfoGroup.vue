bba69dc0f9d83cca3d7bf6273865bec73d99fbfd
Nicolas Pucci <nicolaspucci1989@gmail.com>
2023-01-07 12:04:51 -0300

    scaffold product delivery info group

---
+++ src/components/ProductDeliveryInfoGroup.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="py-4">
+    <ProductDeliveryInfo
+        icon="mdi-package-up"
+    >
+      <div>
+        Retiro gratis en nuestro local
+        <br>
+        Rico 640, Dolores, Prov de Buenos Aires
+      </div>
+    </ProductDeliveryInfo>
+    <ProductDeliveryInfo icon="mdi-truck-fast">
+      <div>
+        Envío a domicilio. Consultenós el costo a su localidad.
+        <br>
+        <span style="font-size: .8rem">
+           Realizamos envíos a Dolores y zona con nuestra propia flota de camiones. LLegamos a Castelli, Lezama, Pila, Chascomús, Gral. Guido, Maipú, Gral. Conesa, Lavalle y el Partido de la Costa. Por otros destinos consulte antes de realizar su compra.
+        </span>
+      </div>
+    </ProductDeliveryInfo>
+  </div>
+</template>
+
+<script>
+import ProductDeliveryInfo from "@/components/ProductDeliveryInfo";
+export default {
+  name: "ProductDeliveryInfoGroup",
+  components: { ProductDeliveryInfo }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/ProductPage.vue
+++ src/components/ProductPage.vue
@@ -1,8 +1,12 @@
 <template>
   <v-container>
+    <v-divider class="mt-6"/>
+    <ProductDeliveryInfoGroup/>
+
     <ProductDescription
         :description="product.description"
     />
+
     <ProductAttributesList
         :attributes="product.attributes"
     />
@@ -11,22 +15,23 @@
 </template>
 
 <script>
-  import RelatedProducts from "@/components/RelatedProducts";
-  import ProductAttributesList from "@/components/ProductAttributesList";
-  import ProductDescription from "@/components/ProductDescription";
+import RelatedProducts from "@/components/RelatedProducts";
+import ProductAttributesList from "@/components/ProductAttributesList";
+import ProductDescription from "@/components/ProductDescription";
+import ProductDeliveryInfoGroup from "@/components/ProductDeliveryInfoGroup";
 
-  export default {
-    name: 'ProductPage',
-    props: {
-      product: {
-        type: Object,
-        required: true
-      }
-    },
-    components: { ProductDescription, ProductAttributesList, RelatedProducts },
-    data: () => ({}),
-    mounted() {
-      console.log(this.product)
+export default {
+  name: 'ProductPage',
+  props: {
+    product: {
+      type: Object,
+      required: true
     }
+  },
+  components: { ProductDeliveryInfoGroup, ProductDescription, ProductAttributesList, RelatedProducts },
+  data: () => ({}),
+  mounted() {
+    console.log(this.product)
   }
+}
 </script>
