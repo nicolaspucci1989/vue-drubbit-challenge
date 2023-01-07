9b418afbe0e5b7999e6f0b397e8346513d739523
Nicolas Pucci <nicolaspucci1989@gmail.com>
2023-01-06 23:45:15 -0300

    api: format product description

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,4 @@
 // https://materialesbasualdo.com.ar/api/catalog/product/039033cd-f096-48ab-9950-0a33a1d2e441
-
 const BASE_URL = 'https://materialesbasualdo.com.ar/api'
 
 export async function getProduct(id) {
@@ -7,11 +6,16 @@ export async function getProduct(id) {
   const responseData = await response.json()
   const product = responseData.data.product
   return {
-    ...product,
-    attributes: getProductAttributes(product)
+    ...responseData,
+    attributes: getProductAttributes(product),
+    description: getProductDescription(product)
   }
 }
 
 function getProductAttributes(product) {
   return product.attrs.map(atr => ({id: atr.id, name: atr.attrKey.k, value: atr.v}))
+}
+
+function getProductDescription(product) {
+  return product.info.templateData
 }
