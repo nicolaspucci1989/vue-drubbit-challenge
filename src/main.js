9debe69aa64a2f9702fe3db3286ac99be001f1ba
Nicolas Pucci <nicolaspucci1989@gmail.com>
2023-01-08 15:43:57 -0300

    install vue portal

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import './scss/main.scss'
+import PortalVue from 'portal-vue'
 
 Vue.config.productionTip = false
+
+Vue.use(PortalVue)
 
 new Vue({
   vuetify,
